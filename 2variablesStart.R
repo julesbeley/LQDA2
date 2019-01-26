4db8640982249204b632b62774903800fc87e7f0
Jules Beley <jules.beley@gmail.com>
2019-01-26 11:59:38 +0100

    Added plot and tested with three variables, removed redundant mean matrix

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -10,9 +10,11 @@ X2 <-
         x2 = rnorm(200, mean = 8, sd = 1),
         class = "white"
     )
-X3 <- data.frame(x1 = rnorm(500, mean = -8, sd = 1),
-                 x2 = rnorm(500, mean = 6, sd = 1),
-                 class = "orange")
+X3 <- data.frame(
+    x1 = rnorm(500, mean = -8, sd = 1),
+    x2 = rnorm(500, mean = 6, sd = 1),
+    class = "orange"
+)
 X <- rbind(X1, X2, X3)
 plot(X$x1[X$class == "black"], X$x2[X$class == "black"], xlim = c(-9, 5), ylim = c(-1, 12))
 points(X$x1[X$class == "white"], X$x2[X$class == "white"], col = "red")
@@ -25,36 +27,41 @@ lda2 <- function(value, data) {
     n_cla <- dim(tab)
     nam <- names(tab)
     pi <- c()
-    mu <- matrix(nrow = 2, ncol = n_cla)
+    mu <- matrix(nrow = n_cla, ncol = 2)
     for (i in (1:n_cla)) {
         pi[i] <- tab[i] / dim(data)[1]
-        mu[1, i] <- mean(data$x1[data$class == nam[i]]) # mean matrix: (class1:x1 ... classn:x1)
-        mu[2, i] <- mean(data$x2[data$class == nam[i]]) #              (class1:x2 ... classn:x2)
+        mu[i, 1] <-
+            mean(data$x1[data$class == nam[i]]) # mean matrix: (class1:x1 ... classn:x1)
+        mu[i, 2] <-
+            mean(data$x2[data$class == nam[i]]) #              (class1:x2 ... classn:x2)
     }
+    names(pi) <- nam
     val <- list()
     dev <- list()
-    means <- matrix(nrow = n_cla, ncol = 2)
     mul <- list()
     ccov <- list()
     for (i in (1:n_cla)) {
         val[[i]] <- matrix(nrow = tab[i], ncol = 2)
-        val[[i]] <- cbind(data$x1[data$class == nam[i]], data$x2[data$class == nam[i]])
+        val[[i]] <-
+            cbind(data$x1[data$class == nam[i]], data$x2[data$class == nam[i]])
         dev[[i]] <- matrix(nrow = tab[i], ncol = 2)
-        means[i,] <- cbind(mean(data$x1[data$class == nam[i]]), mean(data$x2[data$class == nam[i]])) # redundant with mu!
         for (j in (1:tab[i])) {
-            dev[[i]][j,] <- val[[i]][j, ] - means[i, ]
+            dev[[i]][j, ] <- val[[i]][j,] - mu[i,]
         }
         mul[[i]] <- matrix(nrow = 2, ncol = 2)
         ccov[[i]] <- matrix(nrow = 2, ncol = 2)
-        mul[[i]] <- t(dev[[i]]) %*% dev[[i]] 
-        ccov[[i]] <- mul[[i]] / (dim(data)[1] - n_cla) 
+        mul[[i]] <- t(dev[[i]]) %*% dev[[i]]
+        ccov[[i]] <- mul[[i]] / (dim(data)[1] - n_cla)
     }
     dis <- c()
     exp <- c()
     cov <- Reduce('+', ccov)
+    rownames(cov) <- c("x1", "x2")
+    colnames(cov) <- c("x1", "x2")
     inv <- solve(cov)
     for (i in (1:n_cla)) {
-        dis[i] <- t(value) %*% inv %*% mu[, i] - (0.5 %*% t(mu[, i]) %*% inv %*% mu[, i]) + log(pi[i])
+        dis[i] <-
+            t(value) %*% inv %*% mu[i,] - 0.5 %*% t(mu[i,]) %*% inv %*% mu[i,] + log(pi[i])
     }
     prob <- c()
     prob <- exp(dis) / sum(exp(dis))
@@ -63,6 +70,33 @@ lda2 <- function(value, data) {
     score <- max(dis)
     proba <- max(prob)
     out <- as.data.frame(cbind(class, score, proba))
-    return(out)
+    maxx1 <- c()
+    minx1 <- c()
+    maxx2 <- c()
+    minx2 <- c()
+    for (i in (1:n_cla)) {
+        maxx1[i] <- mu[i, 1] + 4 * sqrt(cov[1, 1])
+        minx1[i] <- mu[i, 1] - 4 * sqrt(cov[1, 1])
+        maxx2[i] <- mu[i, 2] + 4 * sqrt(cov[2, 2])
+        minx2[i] <- mu[i, 2] - 4 * sqrt(cov[2, 2])
+    }
+    maxx1 <- max(maxx1)
+    minx1 <- min(minx1)
+    maxx2 <- max(maxx2)
+    minx2 <- min(minx2)
+    col <- heat.colors(n = n_cla)
+    plot(
+        X$x1[X$class == nam[1]],
+        X$x2[X$class == nam[1]],
+        xlim = c(minx1, maxx1),
+        ylim = c(minx2, maxx2),
+        col = col[1]
+    )
+    for (i in (2:n_cla)) {
+        points(X$x1[X$class == nam[i]],
+               X$x2[X$class == nam[i]],
+               col = col[i])
+    }
+    return(list(pi, cov, out))
 }
-lda2(c(-2,5), X)
+lda2(c(-2, 5), X)
