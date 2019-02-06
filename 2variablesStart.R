d85686180333c3f60543eeb6368e8d05980353fb
Jules Beley <jules.beley@gmail.com>
2019-02-06 18:22:36 +0100

    Concave hull to compute decision boundaries!

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -1,27 +1,31 @@
-X0 <-
-    data.frame(
-        x1 = rnorm(400, mean = -12, sd = 1),
-        x2 = rnorm(400, mean = 7, sd = 1),
-        class = "black"
-    )
 X1 <-
     data.frame(
-        x1 = rnorm(200, mean = -7, sd = 1),
-        x2 = rnorm(200, mean = 3, sd = 1),
+        x1 = rnorm(100, mean = -7, sd = 1),
+        x2 = rnorm(100, mean = 3, sd = 1),
         class = "black"
     )
 X2 <-
     data.frame(
         x1 = rnorm(200, mean = -4, sd = 1),
         x2 = rnorm(200, mean = 6, sd = 1),
-        class = "white"
+        class = "blue"
     )
 X3 <- data.frame(
     x1 = rnorm(200, mean = -1, sd = 1),
     x2 = rnorm(200, mean = -5, sd = 1),
     class = "orange"
 )
-X <- rbind(X0, X1, X2, X3) 
+X4 <- data.frame(
+    x1 = rnorm(500, mean = 10, sd = 1),
+    x2 = rnorm(500, mean = -5, sd = 1),
+    class = "red"
+)
+X5 <- data.frame(
+    x1 = rnorm(600, mean = 4, sd = 1),
+    x2 = rnorm(600, mean = 8, sd = 1),
+    class = "white"
+)
+X <- rbind(X1, X2, X3, X4, X5) 
 
 lda2 <- function(data) {
     if (is.numeric(data[, 1] & is.numeric(data[, 2]))) {
@@ -69,8 +73,22 @@ lda2 <- function(data) {
         }
     }
     col <- heat.colors(n = n_cla)
-    runifx1 <- runif(100000, min = min(minx[, 1]), max = max(maxx[, 1]))
-    runifx2 <- runif(100000, min = min(minx[, 2]), max = max(maxx[, 2]))
+    plot(
+        X$x1[X$class == nam[1]],
+        X$x2[X$class == nam[1]],
+        xlim = c(min(minx[, 1]), max(maxx[, 1])),
+        ylim = c(min(minx[, 2]), max(maxx[, 2])),
+        col = col[1],
+        xlab = "X1",
+        ylab = "X2"
+    )
+    for (i in (2:n_cla)) {
+        points(X$x1[X$class == nam[i]],
+               X$x2[X$class == nam[i]],
+               col = col[i])
+    }
+    runifx1 <- runif(100000, min = 1.2 * min(minx[, 1]), max = 1.2 * max(maxx[, 1]))
+    runifx2 <- runif(100000, min = 1.2 * min(minx[, 2]), max = 1.2 * max(maxx[, 2]))
     runif <- cbind(runifx1, runifx2)
     dismc <- matrix(nrow = 100000, ncol = n_cla)
     for (h in (1:100000)) {
@@ -93,61 +111,19 @@ lda2 <- function(data) {
             stringsAsFactors = FALSE
         )
     dismc <- dismc[order(dismc$class), ]
-    plot(
-        dismc$x1[dismc$class == nam[1]],
-        dismc$x2[dismc$class == nam[1]],
-        xlim = c(min(minx[, 1]), max(maxx[, 1])), # x1
-        ylim = c(min(minx[, 2]), max(maxx[, 2])), # x2
-        col = col[1],
-        xlab = "X1",
-        ylab = "X2",
-        pch = 20,
-        cex = 0.5
-    )
-    for (i in (2:n_cla)) {
-        points(dismc$x1[dismc$class == nam[i]],
-               dismc$x2[dismc$class == nam[i]], 
-               col = col[i],
-               pch = 20,
-               cex = 0.5)
-    }
     for (i in (1:n_cla)) {
-        points(mu[i,1], mu[i,2],
-               pch = )
+        points(mu[i,1], mu[i,2])
     }
-    slopes <- matrix(nrow = n_cla, ncol = n_cla) # slope of mean lines between class i and j
-    intercepts <- matrix(nrow = n_cla, ncol = n_cla) # intercept of mean lines "
+    library(concaveman)
+    points <- list()
+    hulls <- list()
     for (i in (1:n_cla)) {
-        for (j in (i:n_cla)) {
-            slopes[i,j] <- (mu[j,2] - mu[i,2]) / (mu[j,1] - mu[i,1])
-            intercepts[i,j] <- mu[i,2] - slopes[i,j] * mu[i,1]
-        }
+        points[[i]] <- cbind(dismc$x1[dismc$class == nam[i]],
+                             dismc$x2[dismc$class == nam[i]])
+        hulls[[i]] <- concaveman(points[[i]], concavity = 10e20)
+        lines(hulls[[i]])
     }
-    x <- seq(mu[1,1], mu[2,1], length = 100)
-    y <- slopes[1,2] * x + intercepts[1,2]
-    xy <- cbind(x,y)
-    dis <- matrix(nrow = 100, ncol = n_cla)
-    for (j in (1:100)) {
-        for (i in (1:n_cla))
-            dis[j,i] <- t(xy[j,]) %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
-    }
-    colnames(dis) <- nam
-    classtest <- c()
-    for (j in (1:100)) {
-        classtest[j] <- colnames(dis)[dis[j,] == max(dis[j,])]
-    }
-    dis <- as.data.frame(cbind(dis,classtest)) # scores and class for every point on the means line
-    if (length(table(dis$classtest)) == 2) {
-        for (j in (1:99)) {
-            if (dis$classtest[j] != dis$classtest[j+1]) {
-                threshold <- j
-            }
-        }
-    }
-    bounds1 <- data.frame(x1 = xy[threshold,1], x2 = xy[threshold,2])
-    bounds2 <- data.frame(x1 = xy[threshold + 1,1], x2 = xy[threshold + 1,2])
-    bounds <- rbind(bounds1, bounds2)
-    rownames(bounds) <- NULL
-    return(list(pi, cov, mu, bounds))
+    return(list(pi, cov, mu))
 }
 lda2(X)
+
