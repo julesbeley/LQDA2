0c4822be947f1d563690ca080e59ad2440004233
Jules Beley <jules.beley@gmail.com>
2019-03-08 00:02:24 +0100

    Some simplifications

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -1,4 +1,7 @@
 # generate random points on the fringes of the normal distribution + near bounding box
+# points still somtimes fall outside of bb
+# concaveman JavaScript V8 TypeError
+# separate graphical part from computation of decision boundary path
 # develop qda function
 
 rm(list = ls())
@@ -7,14 +10,14 @@ classnames <- c("white", "black", "blue", "red", "green", "orange", "purple", "b
 length <- runif(8, 100, 500)
 for (i in (1:runif(1, 3, 8))) {
     Xlist[[i]] <- data.frame(
-        x1 = rnorm(2000, mean = runif(1, -10, 10), sd = 1),
-        x2 = rnorm(2000, mean = runif(1, -10, 10), sd = 1),
+        x1 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
+        x2 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
         class = classnames[i]
     )
 }
 do.call("rbind", Xlist) -> X
 
-lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
+lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
     library(ggplot2)
     library(dplyr)
     if (is.numeric(data[, 1] && is.numeric(data[, 2]))) {
@@ -39,20 +42,19 @@ lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
     ccov <- list()
     for (i in (1:n_cla)) {
         val[[i]] <- matrix(nrow = tab[i], ncol = 2)
-        val[[i]] <- cbind(data$x1[data$class %in% nam[i]], data$x2[data$class %in% nam[i]])
+        val[[i]] <- cbind(data$x1[data$class %in% nam[i]], 
+                          data$x2[data$class %in% nam[i]])
         dev[[i]] <- matrix(nrow = tab[i], ncol = 2)
         for (j in (1:tab[i])) {
             dev[[i]][j, ] <- val[[i]][j, ] - mu[i, ]
         }
-        mul[[i]] <- matrix(nrow = 2, ncol = 2)
         ccov[[i]] <- matrix(nrow = 2, ncol = 2)
-        mul[[i]] <- t(dev[[i]]) %*% dev[[i]]
-        ccov[[i]] <- mul[[i]] / (dim(data)[1] - n_cla)
+        ccov[[i]] <- t(dev[[i]]) %*% dev[[i]] / (dim(data)[1] - n_cla)
     }
     dis <- c()
     exp <- c()
     cov <- Reduce('+', ccov)
-    rm(val, dev, mul, ccov)
+    rm(val, dev, ccov)
     rownames(cov) <- c("x1", "x2")
     colnames(cov) <- c("x1", "x2")
     inv <- solve(cov)
@@ -60,16 +62,16 @@ lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
     minx <- matrix(nrow = n_cla, ncol = 2)
     for (i in (1:n_cla)) {
         for (j in (1:2)) {
-            maxx[i, j] <- mu[i, j] + 4 * sqrt(cov[j, j])
-            minx[i, j] <- mu[i, j] - 4 * sqrt(cov[j, j])
+            maxx[i, j] <- mu[i, j] + 6 * sqrt(cov[j, j])
+            minx[i, j] <- mu[i, j] - 6 * sqrt(cov[j, j])
         }
     }
     ggplot() + 
         geom_point(data = subset(data, class %in% nam[1]),
                    aes(x = x1, y = x2),
                    col = col[1]) +
-        scale_x_continuous(limits = c(1.12 * min(minx[, 1]), 1.12 * max(maxx[, 1]))) +
-        scale_y_continuous(limits = c(1.12 * min(minx[, 2]), 1.12 * max(maxx[, 2]))) +
+        scale_x_continuous(limits = c(1.15 * min(minx[, 1]), 1.15 * max(maxx[, 1]))) +
+        scale_y_continuous(limits = c(1.15 * min(minx[, 2]), 1.15 * max(maxx[, 2]))) +
         xlab("X1") +
         ylab("X2") +
         labs(title = "Observations and predicted decision boundaries") -> g
@@ -83,18 +85,17 @@ lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
                   ...) -> g
     }
     if (all(pi %in% rep.int(pi[1], length(pi)))) {
-        library(ggvoronoi)
         box <- data.frame(x1 = c(min(minx[, 1]),
                                  rep.int(max(maxx[, 1]), 2),
                                  min(minx[, 1])),
                           x2 = c(rep.int(min(minx[, 2]), 2),
                                  rep.int(max(maxx[, 2]), 2)))
-        g + stat_voronoi(data = as.data.frame(mu),
-                         aes(x = x1, y = x2),
-                         geom = "path",
-                         outline = box,
-                         size = 1,
-                         col = "grey30") -> g
+        g + ggvoronoi::stat_voronoi(data = as.data.frame(mu),
+                                    aes(x = x1, y = x2),
+                                    geom = "path",
+                                    outline = box,
+                                    size = 1,
+                                    col = "grey30") -> g
         rm(minx, maxx)
     }
     else { 
@@ -125,13 +126,13 @@ lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
         )
         rm(runifx1, runifx2)
         dismc <- dismc[order(dismc$class), ]
-        points <- list()
         hulls <- list()
         for (i in (1:n_cla)) {
-            points[[i]] <- cbind(dismc$x1[dismc$class %in% nam[i]],
-                                 dismc$x2[dismc$class %in% nam[i]])
-            hulls[[i]] <- concaveman::concaveman(points[[i]], concavity = 10e20)
-            as.data.frame(hulls[[i]]) -> hulls[[i]]
+            hulls[[i]] <- as.data.frame(
+                concaveman::concaveman(cbind(dismc$x1[dismc$class %in% nam[i]],
+                                             dismc$x2[dismc$class %in% nam[i]]), 
+                                       concavity = 10e20)
+                )
             g + geom_path(data = hulls[[i]],
                         aes(x = V1, y = V2),
                         size = 1,
@@ -146,4 +147,4 @@ lda2 <- function(data, nmc = 200000, palette = heat.colors, ...) {
     return(out)
 }
 
-lda2(data = X, nmc = 500000)
+lda2(X, nmc = 1000000)
