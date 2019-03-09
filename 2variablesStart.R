4bf28f112ae93bd550c84d982b247a7ebb0bbeda
Jules Beley <jules.beley@gmail.com>
2019-03-09 09:47:13 +0100

    Progress with sp, managed to define inverse of hulls and tested random sampling

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -15,7 +15,7 @@ for (i in (1:runif(1, 3, 8))) {
 }
 do.call("rbind", Xlist) -> X
 
-lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
+lda2 <- function(data, palette = heat.colors, ...) {
     library(ggplot2)
     if (is.numeric(data[, 1] && is.numeric(data[, 2]))) {
         names(data) <- c("x1", "x2", "class")
@@ -81,12 +81,12 @@ lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
                   panel.background = element_blank(),
                   ...) -> g
     }
+    box <- data.frame(x1 = c(min(minx[, 1]),
+                             rep.int(max(maxx[, 1]), 2),
+                             min(minx[, 1])),
+                      x2 = c(rep.int(min(minx[, 2]), 2),
+                             rep.int(max(maxx[, 2]), 2)))
     if (all(pi %in% rep.int(pi[1], length(pi)))) {
-        box <- data.frame(x1 = c(min(minx[, 1]),
-                                 rep.int(max(maxx[, 1]), 2),
-                                 min(minx[, 1])),
-                          x2 = c(rep.int(min(minx[, 2]), 2),
-                                 rep.int(max(maxx[, 2]), 2)))
         g + ggvoronoi::stat_voronoi(data = as.data.frame(mu),
                                     aes(x = x1, y = x2),
                                     geom = "path",
@@ -96,15 +96,16 @@ lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
         rm(minx, maxx)
     }
     else { 
-        runifx1 <- runif(nmc,
+        library(sp)
+        runifx1 <- runif(1000,
                          min = min(minx[, 1]),
                          max = max(maxx[, 1]))
-        runifx2 <- runif(nmc,
+        runifx2 <- runif(1000,
                          min = min(minx[, 2]),
                          max = max(maxx[, 2]))
         runif <- cbind(runifx1, runifx2)
-        dismc <- matrix(nrow = nmc, ncol = n_cla)
-        for (h in (1:nmc)) {
+        dismc <- matrix(nrow = 1000, ncol = n_cla)
+        for (h in (1:1000)) {
             for (i in (1:n_cla)) {
                 dismc[h, i] <- t(runif[h, ]) %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
             }
@@ -112,7 +113,7 @@ lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
         colnames(dismc) <- nam
         rownames(mu) <- nam
         classmc <- c()
-        for (h in (1:nmc)) {
+        for (h in (1:1000)) {
             classmc[h] <- names(dismc[h, ])[dismc[h, ] %in% max(dismc[h, ])]
         }
         dismc <- data.frame(
@@ -124,32 +125,42 @@ lda2 <- function(data, nmc = 250000, palette = heat.colors, ...) {
         rm(runifx1, runifx2)
         dismc <- dismc[order(dismc$class), ]
         hulls <- list()
+        mtosp <- function(m) SpatialPolygons(list(Polygons(list(Polygon(m)), 1)))
+        box <- mtosp(box)
         for (i in (1:n_cla)) {
             hulls[[i]] <- as.data.frame(
                 concaveman::concaveman(cbind(dismc$x1[dismc$class %in% nam[i]],
                                              dismc$x2[dismc$class %in% nam[i]]),
-                                       concavity = 50)
-                )
-            g + geom_path(data = hulls[[i]],
-                        aes(x = V1, y = V2),
-                        size = 1,
-                        col = "grey30") -> g 
+                                       concavity = 50))
+            hulls[[i]] <- mtosp(hulls[[i]])
+        }
+        rgeos::gDifference(box, hulls[[1]]) -> diff
+        for (i in (2:n_cla)) {
+            rgeos::gDifference(diff, hulls[[i]]) -> diff
         }
     }
     print(g)
-    list(pi, cov, mu) -> out
+    list(pi, cov, mu, diff) -> out
     names(out) <- c("Prior probabilities", 
                     "Covariance matrix", 
-                    "Class means")
+                    "Class means",
+                    "Hulls")
     return(out)
 }
+lda2(X) -> r
 
-# gif test
+plot(r$Hulls)
+spsample(r$Hulls, n = 200, "random")
+points(spsample(r$Hulls, n = 200, "random"))
 
-for (i in seq(from = 50, to = 10050, by = 100)) {
-    png(print(paste("./plot", i , ".png", sep = "")))
-    lda2(X, nmc = i)
-    dev.off()
+
+# plotting section
+
+for (i in (1:n_cla)) {
+    g + geom_path(data = hulls[[i]],
+                  aes(x = V1, y = V2),
+                  size = 1,
+                  col = "grey30") -> g 
 }
 
 # sampling in area outside of a polygon!
@@ -158,14 +169,25 @@ library(sp)
 library(rgeos)
 cbind(c(0, 3, 3, 0, 0), c(0, 0, 3, 3, 0)) -> pol
 cbind(c(0, 5, 5, 0, 0), c(0, 0, 5, 5, 0)) -> pol2
+cbind(c(1, 2, 2, 1, 1), c(1, 1, 2, 2, 1)) -> pol3
 sps <- SpatialPolygons(list(Polygons(list(Polygon(pol)), 1)))
 sps2 <- SpatialPolygons(list(Polygons(list(Polygon(pol2)), 1)))
-plot(sps)
-gDifference(sps2, sps) -> diff
+sps3 <- SpatialPolygons(list(Polygons(list(Polygon(pol3)), 1)))
+
+gDifference(sps2, sps3) -> diff
 plot(diff)
-spsample(diff, n = 20, "random")
-plot(concaveman(spsample(diff, n = 20, "random")))
+points(spsample(diff, n = 2000, "random"))
+
 
 # convert back to data.frame for testing
 sps@polygons[[1]]@Polygons[[1]]@coords -> d
-as.data.frame(d)
+diff@polygons[[1]]@Polygons[[1]]@coords 
+
+
+# gif test
+
+# for (i in seq(from = 50, to = 10050, by = 100)) {
+#    png(print(paste("./plot", i , ".png", sep = "")))
+#    lda2(X, nmc = i)
+#    dev.off()
+#}
