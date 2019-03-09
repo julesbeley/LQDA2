f661ee183194b4b11f81ea5edc930721066e9f9c
Jules Beley <jules.beley@gmail.com>
2019-03-09 16:28:26 +0100

    Removed graphical part and fined-tuned the hulls output

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -1,12 +1,12 @@
-# GENERATE RANDOM POINTS FURTHER THAN BOUNDARY WITH FEWER RANDOM POINTS!!!!!!!
 # separate graphical part from computation of decision boundary path
 # develop qda function
 
 rm(list = ls())
 Xlist <- list()
-classnames <- c("white", "black", "blue", "red", "green", "orange", "purple", "brown")
-length <- runif(8, 100, 500)
-for (i in (1:runif(1, 3, 8))) {
+classnames <- c("white", "black", "blue", "red", "green", "orange", "purple", "brown", "car", "truck",
+                "limo", "coke", "soda")
+length <- runif(13, 100, 500)
+for (i in (1:runif(1, 2, 13))) {
     Xlist[[i]] <- data.frame(
         x1 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
         x2 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
@@ -15,7 +15,7 @@ for (i in (1:runif(1, 3, 8))) {
 }
 do.call("rbind", Xlist) -> X
 
-lda2 <- function(data, palette = heat.colors, ...) {
+lda2 <- function(data, k = 3) {
     library(ggplot2)
     if (is.numeric(data[, 1] && is.numeric(data[, 2]))) {
         names(data) <- c("x1", "x2", "class")
@@ -32,7 +32,7 @@ lda2 <- function(data, palette = heat.colors, ...) {
     }
     names(pi) <- nam
     colnames(mu) <- c("x1", "x2")
-    col <- palette(n = n_cla, alpha = 0.7)
+    col <- heat.colors(n = n_cla, alpha = 0.7)
     val <- list()
     dev <- list()
     mul <- list()
@@ -63,37 +63,23 @@ lda2 <- function(data, palette = heat.colors, ...) {
             minx[i, j] <- mu[i, j] - 6 * sqrt(cov[j, j])
         }
     }
-    ggplot() + 
-        geom_point(data = subset(data, class %in% nam[1]),
-                   aes(x = x1, y = x2),
-                   col = col[1]) +
-        scale_x_continuous(limits = c(1.15 * min(minx[, 1]), 1.15 * max(maxx[, 1]))) +
-        scale_y_continuous(limits = c(1.15 * min(minx[, 2]), 1.15 * max(maxx[, 2]))) +
-        xlab("X1") +
-        ylab("X2") +
-        labs(title = "Observations and predicted decision boundaries") -> g
-    for (i in (2:n_cla)) {
-        g + geom_point(data = subset(data, class %in% nam[i]),
-                       aes(x = x1, y = x2),
-                       col = col[i],
-                       alpha = 0.4) + 
-            theme(plot.title = element_text(hjust = 0.5),
-                  panel.background = element_blank(),
-                  ...) -> g
-    }
     box <- data.frame(x1 = c(min(minx[, 1]),
                              rep.int(max(maxx[, 1]), 2),
                              min(minx[, 1])),
                       x2 = c(rep.int(min(minx[, 2]), 2),
                              rep.int(max(maxx[, 2]), 2)))
     if (all(pi %in% rep.int(pi[1], length(pi)))) {
-        g + ggvoronoi::stat_voronoi(data = as.data.frame(mu),
-                                    aes(x = x1, y = x2),
-                                    geom = "path",
-                                    outline = box,
-                                    size = 1,
-                                    col = "grey30") -> g
+        library(ggvoronoi)
+        voronoi_polygon(data = as.data.frame(mu),
+                        x = "x1", 
+                        y = "x2",
+                        outline = box) -> raw
         rm(minx, maxx)
+        hulls <- list()
+        for (i in (1:n_cla)) {
+            hulls[[i]] <- as.data.frame(raw@polygons[[i]]@Polygons[[1]]@coords)
+            names(hulls[[i]]) <- c("x1", "x2")
+        }
     }
     else { 
         library(sp)
@@ -128,7 +114,7 @@ lda2 <- function(data, palette = heat.colors, ...) {
                                        concavity = 50))
             hulls[[i]] <- mtosp(hulls[[i]])
         }
-        for (k in (1:3)) {
+        for (j in (1:k)) {
             for (i in (1:n_cla)) {
                 suppressWarnings(hulls[[i]] <- mtosp(hulls[[i]]))
             }
@@ -161,29 +147,18 @@ lda2 <- function(data, palette = heat.colors, ...) {
                     concaveman::concaveman(cbind(dismc2$x1[dismc2$class %in% nam[i]],
                                                  dismc2$x2[dismc2$class %in% nam[i]]),
                                            concavity = 50))
+                names(hulls[[i]]) <- c("x1", "x2")
             }
-        }
-        for (i in (1:n_cla)) {
-            g + geom_path(data = hulls[[i]],
-                          aes(x = V1, y = V2),
-                          size = 1,
-                          col = "grey30") -> g 
+            rm(dismc2)
         }
     }
-    print(g)
-    list(pi, cov, mu) -> out
+    list(pi, cov, mu, hulls) -> out
     names(out) <- c("Prior probabilities", 
                     "Covariance matrix", 
-                    "Class means")
+                    "Class means",
+                    "Decision boundaries")
     return(out)
 }
+
 lda2(X)
 
-
-# gif test
-
-# for (i in seq(from = 50, to = 10050, by = 100)) {
-#    png(print(paste("./plot", i , ".png", sep = "")))
-#    lda2(X, nmc = i)
-#    dev.off()
-#}
