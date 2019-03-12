0bb2bb96166d25cda0a116fe3f9f4819b3f1ee75
Jules Beley <jules.beley@gmail.com>
2019-03-12 10:46:57 +0100

    Tabs

--- 2variablesStart.R
+++ 2variablesStart.R
@@ -7,166 +7,166 @@ classnames <- c("white", "black", "blue", "red", "green", "orange", "purple", "b
                 "limo", "coke", "soda")
 length <- runif(13, 100, 500)
 for (i in (1:runif(1, 2, 13))) {
-    Xlist[[i]] <- data.frame(
-        x1 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
-        x2 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
-        class = classnames[i]
-    )
+      Xlist[[i]] <- data.frame(
+            x1 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
+            x2 = rnorm(length[i], mean = runif(1, -10, 10), sd = runif(1, 0.5, 4)),
+            class = classnames[i]
+      )
 }
 do.call("rbind", Xlist) -> X
 
 lda2 <- function(data, k = 3) {
-    library(ggplot2)
-    if (is.numeric(data[, 1] && is.numeric(data[, 2]))) {
-        names(data) <- c("x1", "x2", "class")
-    }
-    tab <- table(data$class)
-    n_cla <- dim(tab)
-    nam <- names(tab)
-    pi <- c()
-    mu <- matrix(nrow = n_cla, ncol = 2)
-    for (i in (1:n_cla)) {
-        pi[i] <- tab[i] / dim(data)[1]
-        mu[i, 1] <- mean(data$x1[data$class %in% nam[i]]) 
-        mu[i, 2] <- mean(data$x2[data$class %in% nam[i]])
-    }
-    names(pi) <- nam
-    colnames(mu) <- c("x1", "x2")
-    col <- heat.colors(n = n_cla, alpha = 0.7)
-    val <- list()
-    dev <- list()
-    mul <- list()
-    ccov <- list()
-    for (i in (1:n_cla)) {
-        val[[i]] <- matrix(nrow = tab[i], ncol = 2)
-        val[[i]] <- cbind(data$x1[data$class %in% nam[i]], 
-                          data$x2[data$class %in% nam[i]])
-        dev[[i]] <- matrix(nrow = tab[i], ncol = 2)
-        for (j in (1:tab[i])) {
-            dev[[i]][j, ] <- val[[i]][j, ] - mu[i, ]
-        }
-        ccov[[i]] <- matrix(nrow = 2, ncol = 2)
-        ccov[[i]] <- t(dev[[i]]) %*% dev[[i]] / (dim(data)[1] - n_cla)
-    }
-    dis <- c()
-    exp <- c()
-    cov <- Reduce('+', ccov)
-    rm(val, dev, ccov)
-    rownames(cov) <- c("x1", "x2")
-    colnames(cov) <- c("x1", "x2")
-    inv <- solve(cov)
-    maxx <- matrix(nrow = n_cla, ncol = 2)
-    minx <- matrix(nrow = n_cla, ncol = 2)
-    for (i in (1:n_cla)) {
-        for (j in (1:2)) {
-            maxx[i, j] <- mu[i, j] + 6 * sqrt(cov[j, j])
-            minx[i, j] <- mu[i, j] - 6 * sqrt(cov[j, j])
-        }
-    }
-    box <- data.frame(x1 = c(min(minx[, 1]),
-                             rep.int(max(maxx[, 1]), 2),
-                             min(minx[, 1])),
-                      x2 = c(rep.int(min(minx[, 2]), 2),
-                             rep.int(max(maxx[, 2]), 2)))
-    if (all(pi %in% rep.int(pi[1], length(pi)))) {
-        library(ggvoronoi)
-        voronoi_polygon(data = as.data.frame(mu),
-                        x = "x1", 
-                        y = "x2",
-                        outline = box) -> raw
-        rm(minx, maxx)
-        hulls <- list()
-        for (i in (1:n_cla)) {
-            hulls[[i]] <- as.data.frame(raw@polygons[[i]]@Polygons[[1]]@coords)
-            names(hulls[[i]]) <- c("x1", "x2")
-        }
-    }
-    else { 
-        library(sp)
-        runif <- cbind(runif(1000, min = min(minx[, 1]), max = max(maxx[, 1])), 
-                       runif(1000, min = min(minx[, 2]), max = max(maxx[, 2])))
-        dismc <- matrix(nrow = 1000, ncol = n_cla)
-        for (h in (1:1000)) {
+      library(ggplot2)
+      if (is.numeric(data[, 1] && is.numeric(data[, 2]))) {
+            names(data) <- c("x1", "x2", "class")
+      }
+      tab <- table(data$class)
+      n_cla <- dim(tab)
+      nam <- names(tab)
+      pi <- c()
+      mu <- matrix(nrow = n_cla, ncol = 2)
+      for (i in (1:n_cla)) {
+            pi[i] <- tab[i] / dim(data)[1]
+            mu[i, 1] <- mean(data$x1[data$class %in% nam[i]]) 
+            mu[i, 2] <- mean(data$x2[data$class %in% nam[i]])
+      }
+      names(pi) <- nam
+      colnames(mu) <- c("x1", "x2")
+      col <- heat.colors(n = n_cla, alpha = 0.7)
+      val <- list()
+      dev <- list()
+      mul <- list()
+      ccov <- list()
+      for (i in (1:n_cla)) {
+            val[[i]] <- matrix(nrow = tab[i], ncol = 2)
+            val[[i]] <- cbind(data$x1[data$class %in% nam[i]], 
+                              data$x2[data$class %in% nam[i]])
+            dev[[i]] <- matrix(nrow = tab[i], ncol = 2)
+            for (j in (1:tab[i])) {
+                  dev[[i]][j, ] <- val[[i]][j, ] - mu[i, ]
+            }
+            ccov[[i]] <- matrix(nrow = 2, ncol = 2)
+            ccov[[i]] <- t(dev[[i]]) %*% dev[[i]] / (dim(data)[1] - n_cla)
+      }
+      dis <- c()
+      exp <- c()
+      cov <- Reduce('+', ccov)
+      rm(val, dev, ccov)
+      rownames(cov) <- c("x1", "x2")
+      colnames(cov) <- c("x1", "x2")
+      inv <- solve(cov)
+      maxx <- matrix(nrow = n_cla, ncol = 2)
+      minx <- matrix(nrow = n_cla, ncol = 2)
+      for (i in (1:n_cla)) {
+            for (j in (1:2)) {
+                  maxx[i, j] <- mu[i, j] + 6 * sqrt(cov[j, j])
+                  minx[i, j] <- mu[i, j] - 6 * sqrt(cov[j, j])
+            }
+      }
+      box <- data.frame(x1 = c(min(minx[, 1]),
+                               rep.int(max(maxx[, 1]), 2),
+                               min(minx[, 1])),
+                        x2 = c(rep.int(min(minx[, 2]), 2),
+                               rep.int(max(maxx[, 2]), 2)))
+      if (all(pi %in% rep.int(pi[1], length(pi)))) {
+            library(ggvoronoi)
+            voronoi_polygon(data = as.data.frame(mu),
+                            x = "x1", 
+                            y = "x2",
+                            outline = box) -> raw
+            rm(minx, maxx)
+            hulls <- list()
             for (i in (1:n_cla)) {
-                dismc[h, i] <- t(runif[h, ]) %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
+                  hulls[[i]] <- as.data.frame(raw@polygons[[i]]@Polygons[[1]]@coords)
+                  names(hulls[[i]]) <- c("x1", "x2")
             }
-        }
-        colnames(dismc) <- nam
-        rownames(mu) <- nam
-        classmc <- c()
-        for (h in (1:1000)) {
-            classmc[h] <- names(dismc[h, ])[dismc[h, ] %in% max(dismc[h, ])]
-        }
-        dismc <- data.frame( # make sure all classes are sampled
-            x1 = runif[, 1], 
-            x2 = runif[, 2],
-            class = classmc,
-            stringsAsFactors = FALSE
-        )
-        dismc <- dismc[order(dismc$class), ]
-        mtosp <- function(m) SpatialPolygons(list(Polygons(list(Polygon(m)), 1)))
-        box <- mtosp(box)
-        hulls <- list()
-        for (i in (1:n_cla)) { # some classes aren't sampled, hence the concaveman error!
-            hulls[[i]] <- as.data.frame(
-                concaveman::concaveman(cbind(dismc$x1[dismc$class %in% nam[i]],
-                                             dismc$x2[dismc$class %in% nam[i]]),
-                                       concavity = 20))
-            hulls[[i]] <- mtosp(hulls[[i]])
-        }
-        for (j in (1:k)) {
-            for (i in (1:n_cla)) {
-                suppressWarnings(hulls[[i]] <- mtosp(hulls[[i]]))
+      }
+      else { 
+            library(sp)
+            runif <- cbind(runif(1000, min = min(minx[, 1]), max = max(maxx[, 1])), 
+                           runif(1000, min = min(minx[, 2]), max = max(maxx[, 2])))
+            dismc <- matrix(nrow = 1000, ncol = n_cla)
+            for (h in (1:1000)) {
+                  for (i in (1:n_cla)) {
+                        dismc[h, i] <- t(runif[h, ]) %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
+                  }
             }
-            rgeos::gDifference(box, hulls[[1]]) -> diff
-            for (i in (2:n_cla)) {
-                rgeos::gDifference(diff, hulls[[i]]) -> diff
+            colnames(dismc) <- nam
+            rownames(mu) <- nam
+            classmc <- c()
+            for (h in (1:1000)) {
+                  classmc[h] <- names(dismc[h, ])[dismc[h, ] %in% max(dismc[h, ])]
             }
-            spsample(diff, n = 2000, "random") -> sample
-            sample@coords -> sample
-            dismc2 <- matrix(nrow = 2000, ncol = n_cla)
-            for (h in (1:2000)) {
-                for (i in (1:n_cla)) {
-                    dismc2[h, i] <- sample[h, ] %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
-                }
+            dismc <- data.frame( # make sure all classes are sampled!
+                  x1 = runif[, 1], 
+                  x2 = runif[, 2],
+                  class = classmc,
+                  stringsAsFactors = FALSE
+            )
+            dismc <- dismc[order(dismc$class), ]
+            mtosp <- function(m) SpatialPolygons(list(Polygons(list(Polygon(m)), 1)))
+            box <- mtosp(box)
+            hulls <- list()
+            for (i in (1:n_cla)) { # some classes aren't sampled, hence the concaveman error!
+                  hulls[[i]] <- as.data.frame(
+                        concaveman::concaveman(cbind(dismc$x1[dismc$class %in% nam[i]],
+                                                     dismc$x2[dismc$class %in% nam[i]]),
+                                               concavity = 20))
+                  hulls[[i]] <- mtosp(hulls[[i]])
             }
-            colnames(dismc2) <- nam
-            classmc2 <- c()
-            for (h in (1:2000)) {
-                classmc2[h] <- names(dismc2[h, ])[dismc2[h, ] %in% max(dismc2[h, ])]
+            for (j in (1:k)) {
+                  for (i in (1:n_cla)) {
+                        suppressWarnings(hulls[[i]] <- mtosp(hulls[[i]]))
+                  }
+                  rgeos::gDifference(box, hulls[[1]]) -> diff
+                  for (i in (2:n_cla)) {
+                        rgeos::gDifference(diff, hulls[[i]]) -> diff
+                  }
+                  spsample(diff, n = 2000, "random") -> sample
+                  sample@coords -> sample
+                  dismc2 <- matrix(nrow = 2000, ncol = n_cla)
+                  for (h in (1:2000)) {
+                        for (i in (1:n_cla)) {
+                              dismc2[h, i] <- sample[h, ] %*% inv %*% mu[i, ] - 0.5 %*% t(mu[i, ]) %*% inv %*% mu[i, ] + log(pi[i])
+                        }
+                  }
+                  colnames(dismc2) <- nam
+                  classmc2 <- c()
+                  for (h in (1:2000)) {
+                        classmc2[h] <- names(dismc2[h, ])[dismc2[h, ] %in% max(dismc2[h, ])]
+                  }
+                  dismc2 <- data.frame(
+                        x1 = sample[, 1], 
+                        x2 = sample[, 2],
+                        class = classmc2,
+                        stringsAsFactors = FALSE
+                  ) 
+                  dismc2 <- dismc2[order(dismc2$class), ]
+                  for (i in (1:n_cla)) {
+                        hulls[[i]] <- as.data.frame(
+                              concaveman::concaveman(cbind(dismc2$x1[dismc2$class %in% nam[i]],
+                                                           dismc2$x2[dismc2$class %in% nam[i]]),
+                                                     concavity = 20))
+                        names(hulls[[i]]) <- c("x1", "x2")
+                  }
+                  rm(dismc2)
             }
-            dismc2 <- data.frame(
-                x1 = sample[, 1], 
-                x2 = sample[, 2],
-                class = classmc2,
-                stringsAsFactors = FALSE
-            ) 
-            dismc2 <- dismc2[order(dismc2$class), ]
-            for (i in (1:n_cla)) {
-                hulls[[i]] <- as.data.frame(
-                    concaveman::concaveman(cbind(dismc2$x1[dismc2$class %in% nam[i]],
-                                                 dismc2$x2[dismc2$class %in% nam[i]]),
-                                           concavity = 20))
-                names(hulls[[i]]) <- c("x1", "x2")
-            }
-            rm(dismc2)
-        }
-    }
-    list(pi, cov, mu, hulls) -> out
-    names(out) <- c("Prior probabilities", 
-                    "Covariance matrix", 
-                    "Class means",
-                    "Decision boundaries")
-    return(out)
+      }
+      list(pi, cov, mu, hulls) -> out
+      names(out) <- c("Prior probabilities", 
+                      "Covariance matrix", 
+                      "Class means",
+                      "Decision boundaries")
+      return(out)
 }
 lda2(X) -> t
 
 ggplot() +
-    geom_path(data = t$`Decision boundaries`[[1]],
-              aes(x = x1, y = x2)) -> r
+      geom_path(data = t$`Decision boundaries`[[1]],
+                aes(x = x1, y = x2)) -> r
 for (i in (1:length(t$`Decision boundaries`))) {
-    r + geom_path(data = t$`Decision boundaries`[[i]],
-                  aes(x = x1, y = x2)) + theme_void() -> r
+      r + geom_path(data = t$`Decision boundaries`[[i]],
+                    aes(x = x1, y = x2)) + theme_void() -> r
 }
 r
 
